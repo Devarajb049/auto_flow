1d8d113c511361f76434d30fa811c8df43753b5f
Bhojanapu Deva Raj <bhojanapudevaraj@gmail.com>
2026-06-26 16:12:27 +0530

    feat: add Testimonials component to showcase client reviews

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -36,7 +36,7 @@ export default function Testimonials() {
         </div>
 
         {/* Testimonials Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 lg:gap-8">
           {reviews.map((review, index) => (
             <article
               key={index}
